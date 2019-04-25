101e996ce2d97c69e23dbb622566b4dc3f088853
Pratik Parmar <prateek18597@gmail.com>
2019-04-25 20:32:11 +0530

    Login Complete

---
+++ src/Info.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pratik
+ */
+public class Info {
+    static String user="root";
+    static String pass="1111";
+    static String currentUserId;
+    static String UserId="";
+    static String Role="";
+    static String TransactionPassword="";
+    static String AccountNo="";
+    static String LastLogin="";
+    static String selectedRecordId="";
+    static String url="jdbc:mysql://172.16.44.7:3306/Banking";
+}
