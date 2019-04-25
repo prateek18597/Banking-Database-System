f81575cba11d004f7c59950259a86c4f64292145
Pratik Parmar <prateek18597@gmail.com>
2019-04-25 20:34:07 +0530

    Delete RegisterCustomer.js

--- Website/js/RegisterCustomer.js
+++
@@ -1,9 +0,0 @@
-function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode;
-    if (charCode != 46 && charCode > 31
-    && (charCode < 48 || charCode > 57))
-        return false;
-
-    return true;
-}
-
